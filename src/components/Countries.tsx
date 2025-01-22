9dd69954af8b5b3fe0b36f19efeabe0e803ee6a4
TheCodingTom <dastolto.t@gmail.com>
2025-01-22 10:11:35 +0100

    fetching data but need to implement typescript

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -1,15 +1,18 @@
 // import React from 'react'
 
-import { useEffect } from "react";
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 
 function Countries() {
+
+  const [countries, setCountries] = useState([])
 
   const getCountries = () => {
     fetch("https://restcountries.com/v3.1/all").then((response) => {
       return response.json()
     }).then((result) => {
       console.log(result);
-      
+      setCountries(result)
     })
   }
 
@@ -21,6 +24,51 @@ function Countries() {
   return (
     <div>
       <h1>World Countries App</h1>
+
+      <div>
+        {countries && countries.map((country) => {
+          return  <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {country.name.common}
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+          
+        })}
+
+
+      </div>
+
+      <div>
+      <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/static/images/cards/contemplative-reptile.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+    </Card>
+      </div>
+      
     </div>
   )
 }
