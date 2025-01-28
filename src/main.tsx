9ae008c678ba9cd790afd79092723f52ac5bcf61
TheCodingTom <dastolto.t@gmail.com>
2025-01-28 11:30:27 +0100

    removed css code from index.css and put everything in App.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import App from "./App";
-
-// import App from './App.tsx'
-// import Books from './components/Books.tsx'
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
