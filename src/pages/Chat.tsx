c46b72de1ab4f68c7a819e49e0e358a17b0e8db6
TheCodingTom <dastolto.t@gmail.com>
2025-02-02 16:09:20 +0100

    function to send messages works!

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,45 +1,90 @@
-import { collection, getDocs, Timestamp } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  orderBy,
+  query,
+  Timestamp,
+} from "firebase/firestore";
 import { Button, Card, FloatingLabel, Form, Stack } from "react-bootstrap";
 import { db } from "../config/firebaseConfig";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
 
 type MessageType = {
   user: string;
   text: string;
   date: Timestamp;
+  id: string;
 };
 
 function Chat() {
+  const { user } = useContext(AuthContext);
   const [messages, setMessages] = useState<MessageType[] | null>(null);
+  const [messageText, setMessageText] = useState<string>("");
+
   const getMessages = async () => {
     const querySnapshot = await getDocs(collection(db, "chat"));
     const messagesArray: MessageType[] = [];
     querySnapshot.forEach((doc) => {
       //   console.log(`${doc.id} => ${doc.data().text}`);
-      const message = doc.data() as MessageType;
+      // const message = doc.data() as MessageType;
+      const message: MessageType = {
+        text: doc.data().text,
+        date: doc.data().date,
+        user: doc.data().user,
+        id: doc.id,
+      };
       messagesArray.push(message);
       setMessages(messagesArray);
     });
     console.log(messagesArray);
   };
 
-  const dateFormat = (seconds:number) => {
-    const formattedDate = new Date(seconds * 1000).toLocaleString()
-    return formattedDate
-  }
- 
+  const dateFormat = (seconds: number) => {
+    const formattedDate = new Date(seconds * 1000).toLocaleString();
+    return formattedDate;
+  };
+
+  const handleTextMessageChange = (e: React.ChangeEvent<HTMLFormElement>) => {
+    console.log(e.target.value);
+    const inputText = e.target.value;
+    setMessageText(inputText);
+  };
+
+  const handleMessageSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    // if no user? - protected route YES
+
+    const newMessage = {
+      text: messageText,
+      date: new Date(),
+      user: user,
+    };
+    const docRef = await addDoc(collection(db, "cities"), newMessage);
+
+    if (!docRef) {
+      throw new Error("something went wrong while sending the message");
+    }
+
+    if (docRef) {
+      console.log("message sent successfully");
+    }
+  };
+
   useEffect(() => {
     getMessages();
   }, []);
 
   return (
     <>
-     <h1>Chat</h1>
+      <h1>Chat</h1>
       <Stack gap={3} className="align-items-center">
         {messages &&
           messages.map((message) => {
             return (
-              <Card style={{ width: "18rem" }} key={message.date.nanoseconds}>
+              <Card style={{ width: "18rem" }} key={message.id}>
                 <Card.Body>
                   <Card.Title>{message.user}</Card.Title>
                   <Card.Subtitle className="mb-2 text-muted">
@@ -51,9 +96,9 @@ function Chat() {
             );
           })}
 
-       
-        <Form>
+        <Form onSubmit={handleMessageSubmit}>
           <FloatingLabel
+            onChange={handleTextMessageChange}
             controlId="floatingInput"
             label="message"
             className="mb-3"
