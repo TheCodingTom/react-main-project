d5259d46ea9ad91354c241f67f133cc17cba9815
TheCodingTom <dastolto.t@gmail.com>
2025-02-05 13:37:40 +0100

    started structuring the home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,6 +3,7 @@
 import { useContext } from "react"
 import { AuthContext } from "../context/AuthContext"
 import logo from "../components/globe.svg"
+import SignModal from "../components/SignModal"
 
 const Home = () => {
 
@@ -10,8 +11,10 @@ const Home = () => {
   return (
     <div className="home-container">
         {user ? <h1>Welcome, {user.email}!</h1> : <h1>Welcome, friend!</h1>}
-
+        {user ? "": "Do you want to discover more about the countries in the world?" }
         <img src={logo} className="logo" alt="" />
+        {user ? <p>Travel around the world with this web app!</p> : <SignModal/>}
+        
     </div>
   )
 }
