b0c05828aa02c62576d6c7b654480c442217fc45
TheCodingTom <dastolto.t@gmail.com>
2025-02-05 13:25:58 +0100

    added logo to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,16 @@
 
 import { useContext } from "react"
 import { AuthContext } from "../context/AuthContext"
+import logo from "../components/globe.svg"
 
 const Home = () => {
 
   const {user} = useContext(AuthContext)
   return (
-    <div>
+    <div className="home-container">
         {user ? <h1>Welcome, {user.email}!</h1> : <h1>Welcome, friend!</h1>}
+
+        <img src={logo} className="logo" alt="" />
     </div>
   )
 }
