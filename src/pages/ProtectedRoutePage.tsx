4067babddf662b5bab181cbe252d6443cb245555
TheCodingTom <dastolto.t@gmail.com>
2025-02-12 14:15:10 +0100

    styled protected route page

--- src/pages/ProtectedRoutePage.tsx
+++ src/pages/ProtectedRoutePage.tsx
@@ -1,9 +1,15 @@
-
+import SignModal from "../components/SignModal"
+import lock from "../images/lock-logo.png"
 
 function ProtectedRoutePage() {
+
+   
   return (
-    <div>
-        <h2>Login to see more</h2>
+    <div className="protected-route-page">
+        <h2>Login to see more!</h2>
+        <p>Sorry, this content is available only for users that already have an account.</p>
+        <img src={lock} className="logo" alt="image of a lock" />
+        <SignModal/>
     </div>
   )
 }
