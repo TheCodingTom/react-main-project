8c9959d1d542d9a254dd5d5dc7b0f598a63bd979
TheCodingTom <dastolto.t@gmail.com>
2025-02-13 13:32:19 +0100

    username is displayed in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,28 +1,64 @@
+
+
+
 // import React from 'react'
 
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../context/AuthContext";
 
 import logo from "../images/logo.png";
 import SignModal from "../components/SignModal";
 import { isUserLogged } from "../utils/AuthUtility";
-
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../config/firebaseConfig";
 
 const Home = () => {
   const { user } = useContext(AuthContext);
-  const isAuth = isUserLogged(user)
+  const isAuth = isUserLogged(user);
+
+  const [username, setUsername] = useState<string | null>(null)
+
+  const getUsername = async () => {
+    const querySnapshot = await getDocs(collection(db, "users"));
+    querySnapshot.forEach((doc) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+
+      if (user && user.id === doc.id ) {
+        const displayName = doc.data().displayName
+        setUsername(displayName)
+
+        if (!displayName) {
+          setUsername("friend")
+        }
+      }
+
+      
+       
+    });
+  };
+
+  useEffect(() => {
+    getUsername();
+  }, [user]);  // Run the effect when the user changes
+
 
   return (
     <div className="home-container">
-      {user ? <h1>Welcome, {user.email}!</h1> : <h1>Welcome, friend!</h1>}
+      {user ? <h1>Welcome, {username}!</h1> : <h1>Welcome, friend!</h1>}
       {isAuth
         ? ""
         : "Do you want to discover more about the countries in the world?"}
       <img src={logo} className="logo" alt="" />
 
-      {isAuth ? <p>Travel around the world with this web app!</p> : <SignModal />}
+      {isAuth ? (
+        <p>Travel around the world with this web app!</p>
+      ) : (
+        <SignModal />
+      )}
     </div>
   );
 };
 
 export default Home;
+
