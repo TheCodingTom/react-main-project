d964b1491e6214a539bdc6d50ba90d3612b33eff
TheCodingTom <dastolto.t@gmail.com>
2025-02-18 10:06:15 +0100

    deploy test

--- src/utils/AuthUtility.ts
+++ src/utils/AuthUtility.ts
@@ -1,7 +1,7 @@
 import { User } from "../types/customTypes";
 
-const isUserLogged = (user?: User | null): boolean => { 
-  return !!user;  // ensures the result is always true for truthy values and false for falsy values
+const isUserLogged = (user: User | null | undefined): boolean => {
+  return !!user;
 };
 
 export { isUserLogged };
