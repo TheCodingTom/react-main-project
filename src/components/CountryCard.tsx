e51d703d95b83880a9ff13d8e0d88888f0623590
TheCodingTom <dastolto.t@gmail.com>
2025-02-19 10:01:50 +0100

    removed like button from country cards for now

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -2,38 +2,12 @@ import { Country } from "../types/customTypes";
 import { Link } from "react-router";
 import { Button, Card } from "react-bootstrap";
 import styles from "../styles/countrycard.module.css";
-import { doc, setDoc } from "firebase/firestore";
-import { db } from "../config/firebaseConfig";
-import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
 
 type CountryCardProps = {
   country: Country;
 };
 
 function CountryCard({ country }: CountryCardProps) {
-  const { user } = useContext(AuthContext);
-  const addLikeDoc = async () => {
-    if (user && country.name.common) {
-      try {
-        const likeRef = doc(
-          db,
-          "likes",
-          user.id,
-          "countries",
-          country.name.common
-        );
-        const userLike = {
-          isLiked: true,
-        };
-        await setDoc(likeRef, userLike);
-        console.log("Liked:", country.name.common);
-      } catch (error) {
-        console.error("Error liking country:", error);
-      }
-    }
-  };
-
   return (
     <div>
       <Card className={styles.countryCard} style={{ width: "18rem" }}>
@@ -52,13 +26,6 @@ function CountryCard({ country }: CountryCardProps) {
             <Link to={`${country.name.common}`}>
               <Button variant="primary">Discover more</Button>
             </Link>
-            {user ? (
-              <Button onClick={addLikeDoc} variant="primary">
-                Like
-              </Button>
-            ) : (
-              ""
-            )}
           </div>
         </Card.Body>
       </Card>
